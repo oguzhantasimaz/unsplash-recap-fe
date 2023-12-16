613dff54d2a43f0590ed614014c45496b676e338
Oguzhan Tasimaz <oguzhantasimaz@gmail.com>
2023-12-16 16:39:26 +0300

    fix: icon locations when captured, add: one more div to result to not capture download button

--- src/components/Result.js
+++ src/components/Result.js
@@ -11,13 +11,15 @@ export default function Result() {
   const divRef = useRef(null);
 
   return (
-    <div className="mt-10 grid gap-10 md:mt-16 md:gap-16" ref={divRef}>
-      <ResultStatistic />
+    <div>
+      <div className="mt-10 grid gap-10 p-5 md:mt-16 md:gap-16" ref={divRef}>
+        <ResultStatistic />
 
-      <div className="col-span-1 grid grid-cols-1 gap-10 md:grid-cols-2">
-        {recap.top_photos.map((photo) => {
-          return <ResultImage photo={photo} key={photo.slug} />;
-        })}
+        <div className="col-span-1 grid grid-cols-1 gap-10 md:grid-cols-2">
+          {recap.top_photos.map((photo) => {
+            return <ResultImage photo={photo} key={photo.slug} />;
+          })}
+        </div>
       </div>
 
       <ResultDownload onClick={() => handleCaptureScreenshot(divRef.current)} />
